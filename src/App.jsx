b57cdf0b8227985cee447fb9ecdffea390dfb165
root <root@Eltis>
2024-05-14 10:48:06 -0300

    Feat:add global styles and fonts

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './styles/global.css'
+
+
+function Myname(){
+  return(
+    <h2>André Ferreira</h2>
+  )
+}
+
+function App() {
+ 
+  return ( 
+     <>
+    <h1>Hello,world</h1>
+    <Myname/> 
+    </>
+  )
+
+
+}
+
+export default App
